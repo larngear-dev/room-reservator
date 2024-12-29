7a4c1ab047f7ab079daaf22cc44d8f2ff95a1a07
betich <betich365@gmail.com>
2024-12-29 18:51:27 +0700

    chore(lint): properly set up and configure eslint

--- packages/database/tsup.config.ts
+++ packages/database/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, type Options } from "tsup";
 
 export default defineConfig((options: Options) => ({
-  entryPoints: ["index.ts"],
+  entryPoints: ["src/*/index.ts", "src/index.ts"],
   clean: false,
   dts: true,
   format: ["cjs"],
