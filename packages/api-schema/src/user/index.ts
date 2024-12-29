931a7c88695a6749cdcd901da8e1193bc3191508
betich <betich365@gmail.com>
2024-12-29 14:59:07 +0700

    feat: add database package

--- packages/api-schema/src/user/index.ts
+++ packages/api-schema/src/user/index.ts
@@ -1,1 +1,40 @@
-export {};
+class User {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  role: string;
+  createdAt: string;
+  updatedAt: string;
+  deletedAt: string;
+
+  constructor(user: User) {
+    this.id = user.id;
+    this.name = user.name;
+    this.email = user.email;
+    this.password = user.password;
+    this.role = user.role;
+    this.createdAt = user.createdAt;
+    this.updatedAt = user.updatedAt;
+    this.deletedAt = user.deletedAt;
+  }
+
+  static fromJSON(user: User): User {
+    return new User(user);
+  }
+
+  static toJSON(user: User): User {
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      role: user.role,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+      deletedAt: user.deletedAt,
+    };
+  }
+}
+
+export { User };
