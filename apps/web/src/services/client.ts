c4e6d3a40ea484804f7f965d5bb5c1c65b81061d
betich <betich365@gmail.com>
2025-01-31 14:59:22 +0700

    feat: update

--- apps/web/src/services/client.ts
+++ apps/web/src/services/client.ts
@@ -1,37 +1,37 @@
 import { initClient } from "@ts-rest/core";
-import { contract } from "../../../api/src/contract";
+// import { contract } from "@repo/api/src/contract";
 
 // `contract` is the AppRouter returned by `c.router`
-const client = initClient(contract, {
-  baseUrl: "http://localhost:4001",
-  baseHeaders: {},
-});
+// const client = initClient(contract, {
+//   baseUrl: "http://localhost:4001",
+//   baseHeaders: {},
+// });
 
-export async function getPosts() {
-  const { body, status } = await client.getPosts();
+// export async function getPosts() {
+//   const { body, status } = await client.getPosts();
 
-  if (status === 200) {
-    // body is Post[]c
-    return body;
-  } else {
-    // body is unknown
-    return [];
-  }
-}
+//   if (status === 200) {
+//     // body is Post[]c
+//     return body;
+//   } else {
+//     // body is unknown
+//     return [];
+//   }
+// }
 
-export async function createPost() {
-  const { body, status } = await client.createPost({
-    body: {
-      title: "Post Title",
-      body: "Post Body",
-    },
-  });
+// export async function createPost() {
+//   const { body, status } = await client.createPost({
+//     body: {
+//       title: "Post Title",
+//       body: "Post Body",
+//     },
+//   });
 
-  if (status === 201) {
-    // body is Post
-    return body;
-  } else {
-    // body is unknown
-    return [];
-  }
-}
+//   if (status === 201) {
+//     // body is Post
+//     return body;
+//   } else {
+//     // body is unknown
+//     return [];
+//   }
+// }
